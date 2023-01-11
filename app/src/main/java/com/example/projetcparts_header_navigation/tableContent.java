b728ad362127ca3ce2a4927469b35300ed8aab62
SilverRSt <ralist@mail.bg>
2023-01-11 16:36:31 +0200

    table
    table auto generation

--- app/src/main/java/com/example/projetcparts_header_navigation/tableContent.java
+++ app/src/main/java/com/example/projetcparts_header_navigation/tableContent.java
@@ -1,8 +1,10 @@
 package com.example.projetcparts_header_navigation;
 
 import android.content.Intent;
+import android.content.res.Resources;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
@@ -15,33 +17,46 @@ import android.widget.Toast;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
 public class tableContent extends AppCompatActivity {
     private databaseHelper databaseHelper;
     private SQLiteDatabase database;
+    private final ViewGroup.LayoutParams tableRowDataParams = new TableRow.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT, 1);
+    private static final String TEST_DATA = "test";
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.table_content);
+        this.databaseHelper = new databaseHelper(this);
+        //this.database = databaseHelper.getWritableDatabase();
 
-//        this.databaseHelper = new databaseHelper(this);
-//        this.database = databaseHelper.getWritableDatabase();
+        int tableSize = 0;
 
         TableLayout tableLayout = (TableLayout) findViewById(R.id.table_data);
+        this.generateRows(tableLayout, 3);
 
-        for (int i = 0; i < 2; i++) {
+    }
+
+    public void generateRows(TableLayout tableLayout, final int tableSize) {
+        for (int i = 0; i < tableSize; i++) {
             TableRow row = new TableRow(this);
+            row.setBackgroundColor(ContextCompat.getColor(this, R.color.tableContent));
+            row.setPadding(13, 13, 13, 13);
 
 
             TextView one = new TextView(this);
-            one.setText(R.string.rowDataTest);
+            one.setText(TEST_DATA);
+            one.setLayoutParams(this.tableRowDataParams);
 
             TextView two = new TextView(this);
-            two.setText(R.string.rowDataTest);
+            two.setText(TEST_DATA);
+            two.setLayoutParams(tableRowDataParams);
 
             TextView three = new TextView(this);
-            three.setText(R.string.rowDataTest);
+            three.setText(TEST_DATA);
+            three.setLayoutParams(tableRowDataParams);
 
 
             row.addView(one);
@@ -50,7 +65,6 @@ public class tableContent extends AppCompatActivity {
 
             tableLayout.addView(row, new TableLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT));
         }
-
     }
 
     @Override
